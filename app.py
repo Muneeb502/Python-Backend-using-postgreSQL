9f204a1f511186d77e0fd93336a5e8c5da1bb4df
Muneeb ur Rehman <136859466+Muneeb502@users.noreply.github.com>
2024-06-03 23:37:48 +0500

    added task route

--- app.py
+++ app.py
@@ -11,17 +11,25 @@ app.config["SQLAlCHEMY_DATABASE_URL"] = "postgresql://postgres:root@localhost:54
 db = app(SQLAlchemy)
 
 
-class task(db.model):
+class task(db.Model):
     id = db.Column(db.Integer, primary_key=True,autoincrement = True)
     tittle = db.Column(db.string(200), nullable = False)
     course = db.Column(db.string(200), nullable = False)
     fee = db.Column(db.Boolean, default = False)
-
+with app.app_context():
+    db.creat_all()
    
 
 @app.route("/")
 def home():
     return jsonify({"name": "yes your application is working"})
+
+
+@app.route("/tasks")
+def showtask():
+    tasks = task.query.all()
+    task_list = [ {"id":task.id, "tittle": task.tittle, "course": task.course,"fee": task.fee} for task in tasks]
+    return jsonify({"tasks are shoe": task_list})
 
 
 if __name__ == "__main__":

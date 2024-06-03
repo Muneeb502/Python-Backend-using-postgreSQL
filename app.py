ea4b54dda28c3e467f81cb6bc5bf6d1f3bb666fa
Muneeb ur Rehman <136859466+Muneeb502@users.noreply.github.com>
2024-06-03 23:52:34 +0500

    added some code

--- app.py
+++ app.py
@@ -1,37 +1,30 @@
-from flask import Flask ,request,jsonify
-
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 
-app.config["SQLAlCHEMY_DATABASE_URL"] = "postgresql://postgres:root@localhost:5433/flask_database"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:root@localhost:5433/flask_database"
 
+db = SQLAlchemy(app)
 
-db = app(SQLAlchemy)
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(200), nullable=False)
+    course = db.Column(db.String(200), nullable=False)
+    fee = db.Column(db.Boolean, default=False)
 
-
-class task(db.Model):
-    id = db.Column(db.Integer, primary_key=True,autoincrement = True)
-    tittle = db.Column(db.string(200), nullable = False)
-    course = db.Column(db.string(200), nullable = False)
-    fee = db.Column(db.Boolean, default = False)
 with app.app_context():
-    db.creat_all()
-   
+    db.create_all()
 
 @app.route("/")
 def home():
     return jsonify({"name": "yes your application is working"})
 
-
 @app.route("/tasks")
 def showtask():
-    tasks = task.query.all()
-    task_list = [ {"id":task.id, "tittle": task.tittle, "course": task.course,"fee": task.fee} for task in tasks]
-    return jsonify({"tasks are shoe": task_list})
-
+    tasks = Task.query.all()
+    task_list = [{"id": task.id, "title": task.title, "course": task.course, "fee": task.fee} for task in tasks]
+    return jsonify({"tasks are shown": task_list})
 
 if __name__ == "__main__":
     app.run(debug=True, port=5021)
-

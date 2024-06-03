32a9e05ce4522d4f1b68fabda0a81a95f5d1d53b
Muneeb ur Rehman <136859466+Muneeb502@users.noreply.github.com>
2024-06-03 23:04:34 +0500

    added model classs

--- app.py
+++ app.py
@@ -8,6 +8,17 @@ app = Flask(__name__)
 app.config["SQLAlCHEMY_DATABASE_URL"] = "postgresql://postgres:root@localhost:5433/flask_database"
 
 
+db = app(SQLAlchemy)
+
+
+class task(db.model):
+    id = db.Column(db.Integer, primary_key=True,autoincrement = True)
+    tittle = db.Column(db.string(200), nullable = False)
+    course = db.Column(db.string(200), nullable = False)
+    fee = db.Column(db.Boolean, default = False)
+
+   
+
 @app.route("/")
 def home():
     return jsonify({"name": "yes your application is working"})
